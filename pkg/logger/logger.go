64977d15db47c457d7f8cb8408f2c42067e34c32
L1566 <185537890@qq.com>
2026-04-05 18:57:56 +0800

    完成阶段0-基础设施搭建

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -39,3 +39,35 @@ func Info(args ...interface{})  { Log.Info(args...) }
 func Warn(args ...interface{})  { Log.Warn(args...) }
 func Error(args ...interface{}) { Log.Error(args...) }
 func Fatal(args ...interface{}) { Log.Fatal(args...) }
+
+// ---------- 格式化版本 ----------
+
+func Debugf(format string, args ...interface{}) {
+	Log.Debugf(format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	Log.Infof(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	Log.Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	Log.Errorf(format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	Log.Fatalf(format, args...)
+}
+
+// ---------- 带字段的结构化日志（可选）----------
+
+func WithField(key string, value interface{}) *logrus.Entry {
+	return Log.WithField(key, value)
+}
+
+func WithFields(fields map[string]interface{}) *logrus.Entry {
+	return Log.WithFields(fields)
+}
